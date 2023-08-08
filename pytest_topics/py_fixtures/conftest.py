f0ac4012b40c829c34e694d72475218cbc986e68
Santiago Bello <santiago.bello@mercadolibre.com.co>
2023-08-07 22:46:42 -0500

    seccion 6 fixtures scope

--- pytest_topics/py_fixtures/conftest.py
+++ pytest_topics/py_fixtures/conftest.py
@@ -9,13 +9,14 @@ def setup01():
     wk = pytest.weekdays1.copy()
     wk.append("thur")
     yield wk
-    print("\n Fixture setup01 closing")
+    print("\n Fixture setup01 closing \n")
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def setup02():
     wk2 = pytest.weekdays2.copy()
     wk2.insert(0, "thur")
     yield wk2
+    print("\n Fixture setup02 closing \n")
 
 @pytest.fixture()
 def setup04(request):
